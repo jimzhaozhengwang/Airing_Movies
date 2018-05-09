1ddc5bdd113f9239c145ad931014bb502fbf4e9e
Jim Wang <zhawang@zynga.com>
2018-05-09 13:58:10 -0400

    supported heterogenous layouts inside RecyclerView

--- app/src/main/java/com/codepath/android/codepathandroidflicks/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/android/codepathandroidflicks/adapter/RecyclerViewAdapter.java
@@ -1,8 +1,8 @@
 package com.codepath.android.codepathandroidflicks.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +16,13 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Movie> mDataList;
     private LayoutInflater inflater;
     private Context context;
-    public final String BASE_URL = "https://image.tmdb.org/t/p/w500/";
+    private final int STANDARD = 0;
+    private final int POPULAR = 1;
 
     public RecyclerViewAdapter(Context context, List<Movie> data) {
         this.context = context;
@@ -30,49 +31,51 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.recycler_view_item, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == POPULAR){
+            View view = inflater.inflate(R.layout.recycler_view_item_popular, parent, false);
+            return new PopularViewHolder(view);
+        }
+        View view = inflater.inflate(R.layout.recycler_view_item_standard, parent, false);
+        return new StandardViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, final int position) {
         Movie current = mDataList.get(position);
-        holder.title.setText(current.getTitle());
-        holder.overview.setText(current.getOverview());
 
-        String imagePath = "";
-        int orientation = context.getResources().getConfiguration().orientation;
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            imagePath = BASE_URL + current.getPoster_path();
-        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            imagePath = BASE_URL + current.getBackdrop_path();
+        if (holder.getItemViewType() == POPULAR){
+            final String BASE_URL = "https://image.tmdb.org/t/p/original/";
+            PopularViewHolder popularViewHolder = (PopularViewHolder) holder;
+            String imagePath = BASE_URL + current.getBackdrop_path();
+            Picasso.get()
+                    .load(imagePath)
+                    .fit()
+                    .placeholder(R.drawable.ic_file_download_black_200dp)
+                    .error(R.drawable.ic_file_download_black_200dp)
+                    .into(popularViewHolder.image);
+
+        }else{
+            final String BASE_URL = "https://image.tmdb.org/t/p/w500/";
+            StandardViewHolder standardViewHolder = (StandardViewHolder) holder;
+            standardViewHolder.title.setText(current.getTitle());
+            standardViewHolder.overview.setText(current.getOverview());
+
+            String imagePath = "";
+            int orientation = context.getResources().getConfiguration().orientation;
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                imagePath = BASE_URL + current.getPoster_path();
+            } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imagePath = BASE_URL + current.getBackdrop_path();
+            }
+
+            Picasso.get()
+                    .load(imagePath)
+                    .placeholder(R.drawable.ic_file_download_black_200dp)
+                    .error(R.drawable.ic_file_download_black_200dp)
+                    .into(standardViewHolder.image);
         }
 
-        Picasso.get()
-                .load(imagePath)
-                .placeholder(R.drawable.ic_file_download_black_200dp)
-                .error(R.drawable.ic_file_download_black_200dp)
-                .into(holder.image);
-
-//        holder.image.setImageResource;
-
-//        holder.image.setImageResource(current.getImageID());
-//        holder.title.setText(current.getTitle());
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (holder.getAdapterPosition() == 0){ // information
-//                    ((MainActivity) context).go_to_information();
-//
-//                    // from http://stackoverflow.com/questions/12142255/call-activity-method-from-adapter
-//                    // in the future, see if there are other faster alternatives
-//
-//                }else if (holder.getAdapterPosition() == 1){ // settings
-//                    ((MainActivity) context).go_to_settings();
-//                }
-//            }
-//        });
     }
 
     @Override
@@ -80,17 +83,35 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return mDataList.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        if (mDataList.get(position).getVote_average() >= 7.0){
+            return POPULAR;
+        }
+        return STANDARD;
+    }
+
+    class StandardViewHolder extends RecyclerView.ViewHolder {
 
         ImageView image;
         TextView title;
         TextView overview;
 
-        private ViewHolder(View itemView) {
+        private StandardViewHolder(View itemView) {
             super(itemView);
-            image = itemView.findViewById(R.id.image);
+            image = itemView.findViewById(R.id.standard_image);
             title = itemView.findViewById(R.id.title);
             overview = itemView.findViewById(R.id.overview);
+        }
+    }
+
+    class PopularViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+
+        public PopularViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.popular_image);
         }
     }
 
