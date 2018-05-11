368fb5571cdf5e0f5794afba0e2b92e3453ca395
Jim Wang <zhawang@zynga.com>
2018-05-11 11:03:24 -0400

    added play icon overlay

--- app/src/main/java/com/codepath/android/codepathandroidflicks/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/android/codepathandroidflicks/adapter/RecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -67,70 +68,80 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
         if (holder.getItemViewType() == POPULAR) {
             final String BASE_URL = "https://image.tmdb.org/t/p/original/";
-            PopularViewHolder popularViewHolder = (PopularViewHolder) holder;
+            final PopularViewHolder popularViewHolder = (PopularViewHolder) holder;
             String imagePath = BASE_URL + current.getBackdrop_path();
             Picasso.get()
                     .load(imagePath)
                     .fit()
                     .placeholder(R.drawable.ic_file_download_black_200dp)
                     .error(R.drawable.ic_error_black_200dp)
-                    .into(popularViewHolder.image);
-
-            popularViewHolder.recyclerViewItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    final String THE_MOVIE_DB_API_KEY = "47da6cf417ae9a89d8888c3a8da389d0";
-
-                    OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder()
-                            .url("https://api.themoviedb.org/3/movie/" + current.getId() + "/videos?api_key=" + THE_MOVIE_DB_API_KEY + "&language=en-US")
-                            .build();
-
-                    client.newCall(request).enqueue(new Callback() {
+                    .into(popularViewHolder.movieImage, new com.squareup.picasso.Callback(){
                         @Override
-                        public void onFailure(Call call, IOException e) {
-                            e.printStackTrace();
+                        public void onSuccess() {
+                            popularViewHolder.playIcon.setVisibility(View.VISIBLE);
+
+                            popularViewHolder.recyclerViewItem.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    final String THE_MOVIE_DB_API_KEY = "47da6cf417ae9a89d8888c3a8da389d0";
+
+                                    OkHttpClient client = new OkHttpClient();
+                                    Request request = new Request.Builder()
+                                            .url("https://api.themoviedb.org/3/movie/" + current.getId() + "/videos?api_key=" + THE_MOVIE_DB_API_KEY + "&language=en-US")
+                                            .build();
+
+                                    client.newCall(request).enqueue(new Callback() {
+                                        @Override
+                                        public void onFailure(Call call, IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        @Override
+                                        public void onResponse(Call call, final Response response) throws IOException {
+                                            if (!response.isSuccessful()) {
+                                                throw new IOException("Unexpected code " + response);
+                                            } else {
+                                                try {
+                                                    String jsonData = response.body().string();
+                                                    JSONObject jsonObject = new JSONObject(jsonData);
+                                                    JSONArray resultsArray = jsonObject.getJSONArray("results");
+                                                    Gson gson = new GsonBuilder().create();
+                                                    mVideoList = Arrays.asList(gson.fromJson(resultsArray.toString(),
+                                                            Video[].class));
+
+                                                    // find the first valid trailer
+                                                    for (int c = 0; c < mVideoList.size(); c++) {
+                                                        final Video current = mVideoList.get(c);
+                                                        if (current.getSite().equals(mContext.getString(R.string.YouTube)) &&
+                                                                current.getType().equals(mContext.getString(R.string.Trailer))) {
+
+                                                            Intent intent = new Intent(mContext, PopularMoviePlaybackActivity.class);
+                                                            intent.putExtra("url", current.getKey());
+                                                            mContext.startActivity(intent);
+                                                            break;
+                                                        }
+                                                    }
+                                                } catch (IOException e) {
+                                                    e.printStackTrace();
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+
+                                        }
+                                    });
+                                }
+                            });
                         }
                         @Override
-                        public void onResponse(Call call, final Response response) throws IOException {
-                            if (!response.isSuccessful()) {
-                                throw new IOException("Unexpected code " + response);
-                            } else {
-                                try {
-                                    String jsonData = response.body().string();
-                                    JSONObject jsonObject = new JSONObject(jsonData);
-                                    JSONArray resultsArray = jsonObject.getJSONArray("results");
-                                    Gson gson = new GsonBuilder().create();
-                                    mVideoList = Arrays.asList(gson.fromJson(resultsArray.toString(),
-                                            Video[].class));
-
-                                    // find the first valid trailer
-                                    for (int c = 0; c < mVideoList.size(); c++) {
-                                        final Video current = mVideoList.get(c);
-                                        if (current.getSite().equals(mContext.getString(R.string.YouTube)) &&
-                                                current.getType().equals(mContext.getString(R.string.Trailer))) {
-
-                                            Intent intent = new Intent(mContext, PopularMoviePlaybackActivity.class);
-                                            intent.putExtra("url", current.getKey());
-                                            mContext.startActivity(intent);
-                                            break;
-                                        }
-                                    }
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
+                        public void onError(Exception e) {
 
                         }
                     });
-                }
-            });
+
+
         } else {
             final String BASE_URL = "https://image.tmdb.org/t/p/w500/";
-            StandardViewHolder standardViewHolder = (StandardViewHolder) holder;
+            final StandardViewHolder standardViewHolder = (StandardViewHolder) holder;
             standardViewHolder.title.setText(current.getTitle());
             standardViewHolder.overview.setText(current.getOverview());
 
@@ -146,17 +157,25 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
                     .load(imagePath)
                     .placeholder(R.drawable.ic_file_download_black_200dp)
                     .error(R.drawable.ic_error_black_200dp)
-                    .into(standardViewHolder.image);
+                    .into(standardViewHolder.image, new com.squareup.picasso.Callback(){
+                        @Override
+                        public void onSuccess() {
+                            standardViewHolder.recyclerViewItem.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Gson gson = new Gson();
+                                    Intent intent = new Intent(mContext, MovieDetailsActivity.class);
+                                    intent.putExtra("movieObject", gson.toJson(current));
+                                    mContext.startActivity(intent);
+                                }
+                            });
+                        }
 
-            standardViewHolder.recyclerViewItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Gson gson = new Gson();
-                    Intent intent = new Intent(mContext, MovieDetailsActivity.class);
-                    intent.putExtra("movieObject", gson.toJson(current));
-                    mContext.startActivity(intent);
-                }
-            });
+                        @Override
+                        public void onError(Exception e) {
+
+                        }
+                    });
         }
     }
 
@@ -191,12 +210,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     class PopularViewHolder extends RecyclerView.ViewHolder {
 
-        LinearLayout recyclerViewItem;
-        ImageView image;
+        CoordinatorLayout recyclerViewItem;
+        ImageView movieImage;
+        ImageView playIcon;
 
         public PopularViewHolder(View itemView) {
             super(itemView);
-            image = itemView.findViewById(R.id.popular_image);
+            movieImage = itemView.findViewById(R.id.popular_image);
+            playIcon = itemView.findViewById(R.id.play_icon);
             recyclerViewItem = itemView.findViewById(R.id.recyclerViewItem);
         }
     }
